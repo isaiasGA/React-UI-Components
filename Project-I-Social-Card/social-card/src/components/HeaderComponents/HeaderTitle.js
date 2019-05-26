f47be1a07234b4882a08c1c252e5132cbdc5f309
IsaiasGA <isaiasgarciaariza@gmail.com>
2019-05-26 11:25:18 -0700

    Header title and thumbnail have been added and are now being displayed

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <div className='title'>
+            <h1>LambdaSchool</h1>
+        </div>
+    )
+}
+
+export default HeaderTitle;
